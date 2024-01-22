52234617badbfa074947f8cec65d5574783c1b80
Nihar Hegde <niharhegde163@gmail.com>
2024-01-22 20:18:11 +0530

    basic express hello world

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,12 @@
-import fs from "fs";
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
+app.listen(port, () => {
+  console.log(`Server Listning on port ${port} `);
+});
