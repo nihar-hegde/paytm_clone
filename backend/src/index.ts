3a781fe81805e38c01aaca7abdde7f3572707116
Nihar Hegde <niharhegde163@gmail.com>
2024-01-23 10:55:32 +0530

    fixied minor mistake improper import so  it was not working

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import userRouter from "./routes/user";
+import mainRooter from "./routes/index";
 import cors from "cors";
 import dotenv from "dotenv";
 
@@ -8,8 +8,9 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(cors());
+app.use(express.json());
 
-app.use("/api/v1", userRouter);
+app.use("/api/v1", mainRooter);
 
 app.listen(port, () => {
   console.log(`Server Listning on port ${port} `);
--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -2,8 +2,8 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/user", (req, res) => {
-  res.send("From routes/user.ts Testing Nodemon");
+router.get("/signup", (req, res) => {
+  res.send("Requrest came from index.ts -> router/index.ts -> user.ts");
 });
 
 export default router;
