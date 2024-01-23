f895a66aef11b4e995b5eff1991d308f1451781b
Nihar Hegde <niharhegde163@gmail.com>
2024-01-23 10:21:52 +0530

    minor changes in tscofing and added .env support

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,13 @@
 import express from "express";
 import userRouter from "./routes/user";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+app.use(cors());
 
 app.use("/api/v1", userRouter);
 
--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -3,7 +3,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/user", (req, res) => {
-  res.send("From routes/user.ts");
+  res.send("From routes/user.ts Testing Nodemon");
 });
 
 export default router;
