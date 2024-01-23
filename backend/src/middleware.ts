a6294139638004d4cb05f668d0d68700c93aad21
Nihar Hegde <niharhegde163@gmail.com>
2024-01-23 21:36:20 +0530

    Implemented middleware using typescript

--- backend/src/middleware.ts
+++ backend/src/middleware.ts
@@ -1,1 +1,30 @@
+import jwt from "jsonwebtoken";
 
+interface jwtPayload {
+  userId: string;
+}
+
+const authMiddleware = (req: any, res: any, next: any) => {
+  const jwtSecret = process.env.JWT_SECRET;
+  if (!jwtSecret) {
+    throw new Error("JWT_SECRET Not found");
+  }
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(403).json({});
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, jwtSecret) as jwtPayload;
+
+    req.userId = decoded.userId;
+
+    next();
+  } catch (err) {
+    return res.status(403).json({});
+  }
+};
+export default authMiddleware;
